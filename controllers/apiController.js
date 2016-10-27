45072a5c025de42422ba3ff28e8d8e2882fe65f8
Alicia Brown <cleo.aliciabrown@gmail.com>
2016-10-27 13:52:27 -0700

    update API documentation endpoint

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,8 +1,8 @@
 function index(req, res) {
   res.json({
-    message: "Welcome to tunely!",
-    documentation_url: "https://github.com/tgaff/tunely/api.md",
-    base_url: "http://tunely.herokuapp.com",
+    message: "Welcome to Order Ahead!",
+    documentation_url: "https://github.com/cabrown91/order-ahead",
+    base_url: "http://orderahead.herokuapp.com",
     endpoints: [
       {method: "GET", path: "/api", description: "Describes available endpoints"}
     ]
