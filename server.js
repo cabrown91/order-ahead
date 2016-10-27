3cd2372e80c143480df5d8c97030884b2c761ed6
Alicia Brown <cleo.aliciabrown@gmail.com>
2016-10-27 11:42:04 -0700

    boilerplate server file

---
+++ server.js
@@ -0,0 +1,91 @@
+// SERVER-SIDE JAVASCRIPT
+
+//require express in our app
+var express = require('express');
+// generate a new express app and call it 'app'
+var app = express();
+var bodyParser = require('body-parser');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**********
+ * ROUTES *
+ **********/
+
+/*
+ * HTML Endpoints
+ */
+
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**********
+ * SERVER *
+ **********/
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
