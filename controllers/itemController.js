cdcd5e7adb7e714df60859148790b778b7d3996b
Alicia Brown <cleo.aliciabrown@gmail.com>
2016-11-01 16:34:47 -0700

    add update function

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -31,8 +31,27 @@ function show(req, res) {
   });
 }
 
+function destroy(req, res) {
+  db.Item.findOneAndRemove({ _id: req.params.itemId }, function(err, foundItem){
+    // note you could send just send 204, but we're sending 200 and the deleted entity
+    res.json(foundItem);
+  });
+}
 
-
+function update(req, res) {
+  console.log('updating with data', req.body);
+  db.Items.findById(req.params.itemId, function(err, foundItem){
+    if(err) { console.log('itemsController.update error', err); }
+    foundItem.itemName = req.body.itemName;
+    foundItem.itemPrice = req.body.itemPrice;
+    foundItem.itemDescription = req.body.itemDescription;
+    foundItem.itemImage = req.body.itemImage;
+    foundItem.save(function(err, savedItem) {
+      if(err) { console.log('saving altered item failed'); }
+      res.json(savedItem);
+    });
+  });
+}
 
 
 
@@ -43,5 +62,7 @@ function show(req, res) {
 module.exports = {
   index: index,
   show: show,
-  create: create
+  create: create,
+  destroy: destroy,
+  update: update
 };
