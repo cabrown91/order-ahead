3f633776410ae3d11210f8708e216179c3f431a0
Alicia Brown <cleo.aliciabrown@gmail.com>
2016-11-01 15:25:57 -0700

    add controller to show menu show page views

--- public/scripts/controllers/ItemsIndexController.js
+++ public/scripts/controllers/ItemsIndexController.js
@@ -1,16 +1,22 @@
+console.log('working!!!');
+
+
+
 angular
   .module('order-ahead')
   .controller('ItemsIndexController', ItemsIndexController);
 
-ItemsIndexController.inject = ['$http'];
+ItemsIndexController.inject = ['$http', '$routeParams','$location'];
 
-function ItemsIndexController($http) {
+function ItemsIndexController($http, $routeParams, $location) {
+  var param = $routeParams.id;
+  console.log(param);
   var vm = this;
   vm.newItem = {};
-
+  //
   $http({
     method: 'GET',
-    url: '/api/menu/'
+    url: '/api/menu'
   }).then(function successCallback(response){
     vm.items = response.data;
   }, function errorCallback(response) {
@@ -28,6 +34,17 @@ function ItemsIndexController($http) {
       console.log('There was an error posting the data', data);
     });
   };
+
+
+  $http({
+    method: 'GET',
+    url: '/api/menu/'+$routeParams.id
+  }).then(function successCallback(json) {
+    vm.item = json.data;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
+
 
 
 
