dcc1e447692b1cea1207ee978b042704f37442e2
Alicia Brown <cleo.aliciabrown@gmail.com>
2016-11-01 20:25:38 -0700

    update to connect db to server

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 // SERVER-SIDE JAVASCRIPT
 
 //require express in our app
-var express = require('express');
-// generate a new express app and call it 'app'
-var app = express();
-var bodyParser = require('body-parser');
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    db = require('./models');
 
-app.set('port', (process.env.PORT || 3000));
 
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
-
-
-
+app.use(bodyParser.urlencoded({ extended: true }));
+// need to add this so that we can accept request payloads
+app.use(bodyParser.json());
 
 
 // We'll serve jQuery and bootstrap from a local bower cache avoiding CDNs
@@ -66,6 +65,6 @@ app.get('*', function homepage (req, res) {
  **********/
 
 // listen on port 3000
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
 });
