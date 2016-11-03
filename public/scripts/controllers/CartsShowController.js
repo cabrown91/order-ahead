ce6a63208ec27ba8b0b00271f165a6757d45474e
Alicia Brown <cleo.aliciabrown@gmail.com>
2016-11-03 12:11:22 -0700

    add 'add to cart' function

--- public/scripts/controllers/CartsShowController.js
+++ public/scripts/controllers/CartsShowController.js
@@ -16,12 +16,32 @@ function CartsShowController ($http, $routeParams) {
     console.log('There was an error getting the data', response);
   });
 
+  vm.createItem = function (item) {
+    $http({
+      method: 'PUT',
+      url:'/api/carts/'+$routeParams.id + "/items" +item._id,
+      data: item
+    }).then(function successCallback(json) {
+      ///don't need to do anything
+    }, function errorCallback(response) {
+      console.log('There was an error adding to the cart', response);
+    });
+  };
+
+
 
   vm.deleteItem = function (item) {
     $http({
       method: 'DELETE',
       url:'/api/carts/'+$routeParams.id + "/items" +item._id
-    })
-  }
+    }).then(function successCallback(json) {
+      var index = vm.cart.items.indexOf(item);
+      vm.cart.items.splice(index, 1);
+    }, function errorCallback(response) {
+      console.log('There was an error deleting the data', response);
+    });
+  };
+
+
 
 }
